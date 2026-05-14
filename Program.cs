413b0b70358030a0aefb20ae1c28a59724b9aa41
ashtondscottnz <ashton.dscottnz@gmail.com>
2026-05-15 09:46:44 +1200

    Added teleprinter demonstration

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Teleprinter("""
+                Hello World!
 
+                This is a demonstration of the Teleprinter method
+                that I created a few days earlier and have since implemented
+                into the GraphicsUtils class.
+
+                You should be able to print things to the console in a much 
+                more interesting way now!
+                """, 25, false);
         }
     }
 }
