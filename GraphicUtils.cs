249982869e861a9d431fed3bb44e844a74655f01
ashtondscottnz <ashton.dscottnz@gmail.com>
2026-05-15 09:41:40 +1200

    Added Teleprinter and Teleprinter from file methods

--- GraphicUtils.cs
+++ GraphicUtils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,6 +27,76 @@ namespace ConsoleApp1
                 Console.WriteLine("Error: is your path correct?");
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        public static void Teleprinter(string stringInput, int delay, [Optional] bool cursorVisible)
+        {
+            if (cursorVisible == false)
+            {
+                Console.CursorVisible = false;
+            }
+
+            foreach (string line in stringInput.Split('\n'))
+            {
+                // Ignore comments
+                if (line.StartsWith("//"))
+                {
+                    continue;
+                }
+
+                // Print each character with a delay
+                foreach (char c in line)
+                {
+                    Console.Write(c);
+                    Thread.Sleep(delay);
+                }
+
+                // Print a newline after each line
+                Thread.Sleep(delay);
+                Console.Write("\n");
+            }
+
+            Console.CursorVisible = true;
+        }
+
+        public static void TeleprinterFromFile(string filePath, int delay, [Optional] bool cursorVisible)
+        {
+            if (cursorVisible == false)
+            {
+                Console.CursorVisible = false;
+            }
+
+            try
+            {
+                using StreamReader reader = new(filePath);
+                string textInput = reader.ReadToEnd();
+
+                foreach (string line in textInput.Split('\n'))
+                {
+                    // Ignore comments
+                    if (line.StartsWith("//"))
+                    {
+                        continue;
+                    }
+
+                    // Print each character with a delay
+                    foreach (char c in line)
+                    {
+                        Console.Write(c);
+                        Thread.Sleep(delay);
+                    }
+
+                    // Print a newline after each line
+                    Thread.Sleep(delay);
+                    Console.Write("\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+            }
+
+            Console.CursorVisible = true;
         }
     }
 }
