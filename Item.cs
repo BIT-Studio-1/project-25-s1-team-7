5363ad70a7a0493b541260896b33fcdaaa7fbfed
Kelvin <kelvin.hutchinson2004@gmail.com>
2026-05-15 20:53:40 +1200

    Add Item class shell

---
+++ Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{ // Kelvin:- I created an Item class with two properties, Name and Description, to represent items in the game.
+  // The constructor initializes these properties when an Item object is created.
+    internal class Item 
+    {
+        public string Name { get; set; } 
+        public string Description { get; set; } 
+
+        public Item(string name, string description)
+        {
+            Name = name;
+            Description = description; 
+        }
+    }
+}
