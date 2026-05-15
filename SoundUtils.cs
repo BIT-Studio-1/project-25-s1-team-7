21bee4d3ad30204ee4a91cc388224d85f7ee4452
ashtondscottnz <ashton.dscottnz@gmail.com>
2026-05-15 22:06:32 +1200

    Init SoundUtils class

---
+++ SoundUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // Working on this in just a bit, just setting it up for now...
+    internal class SoundUtils
+    {
+        
+    }
+}
