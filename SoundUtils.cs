cdd78ae83cdb73d39db9b2e220148ab745ef4bf3
HUTCKP1 <HUTCKP1@student.op.ac.nz>
2026-05-18 21:35:53 +1200

    Add PlaySound and StopSound stubs to SoundUtils - food for thought

--- SoundUtils.cs
+++ SoundUtils.cs
@@ -9,6 +9,14 @@ namespace ConsoleApp1
     // Working on this in just a bit, just setting it up for now...
     internal class SoundUtils
     {
-        
+        public static void PlaySound(string filePath) // Just food for thought.
+        {
+            // TODO: implement sound playback
+        }
+
+        public static void StopSound()
+        {
+            // TODO: implement stop sound
+        }
     }
 }
