306af80714c63d7b5c57b87cb744e11533963f8b
Harley Fletcher <flethj1@student.op.ac.nz>
2026-05-21 16:28:41 +1200

    updated move playermove function

--- World.cs
+++ World.cs
@@ -17,6 +17,8 @@ namespace ConsoleApp1
         private int _playerRow = 0;
         private int _playerCol= 1;
 
+
+
         //Call this to display current room via World class ie - "Current room: {world.CurrentRoom}";
         public Room CurrentRoom => _mapGrid[_playerRow, _playerCol];
 
@@ -60,46 +62,31 @@ namespace ConsoleApp1
             //Example of a locked room. Can swap bool value of any room to false to unlock if logic conditions in main script met.
             _mapGrid[1, 0].isLocked = true;
 
-            //Player starting position.
-            _playerCol = 0;
-            _playerRow = 0;
         }
 
         //Returns list of strings of available directions to travel. 
         //Works out if the player is on the border of the map. If they are, that direction is NOT added to the list of available directions to move.
         public List<string> GetAvailableDirections()
         {
+
+            string chosenDirection;
             var directions = new List<string>();
-            //if (_playerRow >= 0)
-            //{
-            //    directions.Add("North");
-            //}
 
-            //if (_playerRow <= 4)
-            //{
-            //    directions.Add("South");
-            //}
-
-            //if (_playerCol >= 0)
-            //{
-            //    directions.Add("West");
-            //}
-
-            //if (_playerCol <= 4)
-            //{
-            //    directions.Add("East");
-
-            //Fixed the above code. Was adding directions to list even when player was on the border of the map. Now checks if player is on the border before adding direction to list.
             if (_playerRow > 0) directions.Add("North");  // room exists above
-            if (_playerRow < 3) directions.Add("South");  // room exists below
+            if (_playerRow < 2) directions.Add("South");  // room exists below
             if (_playerCol > 0) directions.Add("West");   // room exists to the left
-            if (_playerCol < 3) directions.Add("East");   // room exists to the right
+            if (_playerCol < 2) directions.Add("East");   // room exists to the right
 
+            foreach (var direction in directions)
+            {
+                Console.WriteLine(directions.ToString());
+            }
             return directions;
         }
 
-        public bool MovePlayer(string direction)
+        public void MovePlayer(string direction)
         {
+
             //Checks if the direction the player wants to move in is in the list of available directions. If it is, moves player in that direction and returns true. If not, returns false.
             var availableDirections = GetAvailableDirections();
             if (availableDirections.Contains(direction))
@@ -118,10 +105,16 @@ namespace ConsoleApp1
                     case "East":
                         _playerCol++;
                         break;
+                    default:
+                        Console.WriteLine("Invalid input. Please try north, south, east or west.");
+                        return;
                 }
-                return true;
             }
-            return false;
+            else
+            {
+                Console.WriteLine("You walk into a solid stone wall.");
+                return;
+            }
         } // Will need to create a sensitivity check for player input in main script to ensure they are entering directions in the correct format.
           // Could also add a method here to convert player input to the correct format if they enter it in a different way.
     }
