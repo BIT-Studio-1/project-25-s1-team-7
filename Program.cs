388f33ae0a433a7ceb62b4e309268cf1812ce312
ashtondscottnz <ashton.dscott.nz@gmail.com>
2026-05-26 09:24:38 +1200

    Restore TestFile.txt

--- Program.cs
+++ Program.cs
@@ -33,7 +33,7 @@ namespace ConsoleApp1
 
             ConsoleFormatter.Clear();
 
-            Renderer.Render(PathAssets, 1000, true);
+            Renderer.Render(PathAssets + "TestFile.txt", 2500);
 
             ConsoleFormatter.Clear(true);
 
