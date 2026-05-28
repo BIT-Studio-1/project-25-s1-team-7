c5f990509f48e566efbeaa31db8e01e258910652
Tamzin Clarke (1000140833) <CLARTC3@student.op.ac.nz>
2026-05-28 15:28:57 +1200

    TitleScreen additions

--- Titlescreen.cs
+++ Titlescreen.cs
@@ -6,103 +6,92 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Titlescreen
+    public static class Titlescreen
     {
+        enum GameState
+        {
+            TitleScreen,
+            Playing
+        }
+
+        static GameState gameState = GameState.TitleScreen;
+
+        public static void ShowTitleScreen()
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@"                                                                                         
+  *   )    )           (                         (        (                 )  (        
+` )  /( ( /(    (      )\     (   (          (   )\ )     )\      )      ( /(  )\   (   
+ ( )(_)))\())  ))\   (((_)   ))\  )(   (    ))\ (()/(   (((_)  ( /(  (   )\())((_) ))\  
+(_(_())((_)\  /((_)  )\___  /((_)(()\  )\  /((_) ((_))  )\___  )(_)) )\ (_))/  _  /((_) 
+|_   _|| |(_)(_))   ((/ __|(_))(  ((_)((_)(_))   _| |  ((/ __|((_)_ ((_)| |_  | |(_))   
+  | |  | ' \ / -_)   | (__ | || || '_|(_-</ -_)/ _` |   | (__ / _` |(_-<|  _| | |/ -_)  
+  |_|  |_||_|\___|    \___| \_,_||_|  /__/\___|\__,_|    \___|\__,_|/__/ \__| |_|\___|  
+                                                                                        ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" || A Text-based Escape Room ||");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("        Press ENTER to start...");
+
+            Console.ResetColor();
+
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    gameState = GameState.Playing;
+                    break;
+                }
+            }
+        }
+
+        static void RunGame()
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Game started!");
+            Console.ResetColor();
+
+            Console.WriteLine();
+            Console.WriteLine("Your game code goes here.");
+            Console.WriteLine();
+            Console.WriteLine("Press ESC to quit.");
+
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        static void PlayTitleScreen ()
+        {
+            Console.CursorVisible = false;
+
+
+            while (true)
+            {
+                if (gameState == GameState.TitleScreen)
+                {
+                    ShowTitleScreen();
+                }
+                else if (gameState == GameState.Playing)
+                {
+                    RunGame();
+                }
+            }
+        }
     }
 }
-
-
-
-
-/* This is example code, feel free to use or get a feel for how structure could be, you don't have to use it, but might be helpful!
-using System;
-using System.Threading;
-
-public static class TitleScreen
-{
-    enum GameState
-    {
-        TitleScreen,
-        Playing
-    }
-
-    static GameState gameState = GameState.TitleScreen;
-
-    static void Main()
-    {
-        Console.CursorVisible = false;
-
-        while (true)
-        {
-            if (gameState == GameState.TitleScreen)
-            {
-                ShowTitleScreen();
-            }
-            else if (gameState == GameState.Playing)
-            {
-                RunGame();
-            }
-        }
-    }
-
-    public static void ShowTitleScreen()
-    {
-        Console.Clear();
-
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine(@"                                    (                      
-                                   )\ )                   
- (                )           (   (()/(              )    
- )\   (    (   ( /(  `  )    ))\   /(_)) (    (     (     
-((_)  )\   )\  )(_)) /(/(   /((_) (_))   )\   )\    )\  ' 
-| __|((_) ((_)((_)_ ((_)_\ (_))   | _ \ ((_) ((_) _((_))  
-| _| (_-</ _| / _` || '_ \)/ -_)  |   // _ \/ _ \| '  \() 
-|___|/__/\__| \__,_|| .__/ \___|  |_|_\\___/\___/|_|_|_|  
-                    |_|                                   ");
-
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine("             || A Text-based Adventure ||");
-        Console.WriteLine();
-
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine("        Press ENTER to start...");
-
-        Console.ResetColor();
-
-        while (true)
-        {
-            ConsoleKeyInfo key = Console.ReadKey(true);
-
-            if (key.Key == ConsoleKey.Enter)
-            {
-                gameState = GameState.Playing;
-                break;
-            }
-        }
-
-    }
-
-    public static void RunGame()
-    {
-        Console.Clear();
-
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Game started!");
-        Console.ResetColor();
-
-        Console.WriteLine();
-        Console.WriteLine("Your game code goes here.");
-        Console.WriteLine();
-        Console.WriteLine("Press ESC to quit.");
-
-        while (true)
-        {
-            ConsoleKeyInfo key = Console.ReadKey(true);
-
-            if (key.Key == ConsoleKey.Escape)
-            {
-                Environment.Exit(0);
-            }
-        }
-    }
-}*/
