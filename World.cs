478fc5374d270e1c141fc19b106f05a2dd755ff6
ashtondscottnz <ashton.dscott.nz@gmail.com>
2026-06-22 18:58:50 +1200

    Removed unused using directives
    These were really odd actually, why were we using System.Security.Cryptography.X509Certificates?

--- World.cs
+++ World.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
 
 using static ConsoleApp1.GraphicUtils;
 using static ConsoleApp1.Player;
